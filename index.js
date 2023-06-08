8cfc5427e562fea5f48df798ad477a15f21599a5
sayed8901 <sayed91515@gmail.com>
2023-06-08 12:57:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+// moddleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ebwgrc3.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+    const classesCollection = client.db('summerChamp').collection('classes')
+    const instructorsCollection = client.db('summerChamp').collection('instructors')
+
+
+
+    // get all classes
+    app.get('/classes', async (req, res) => {
+        const result = await classesCollection.find().toArray();
+        res.send(result)
+    })
+
+
+    // get all classes based on available seats
+    app.get('/classesByAvailableSeats', async (req, res) => {
+        const result = await classesCollection.find().sort({availableSeats: -1}).toArray();
+        res.send(result)
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Summer Camp is running')
+});
+
+app.listen(port, () => {
+    console.log(`Champ is running at: ${port} kmp`);
+})
